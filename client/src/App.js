961a2b349159969db49f5c3d6c9fdbf6fffb499a
unknown <mrhdigital@yahoo.com>
2018-07-29 13:16:00 -0500

    boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Sharks from "./components/Sharks";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <Switch>
+                <Route exact path="/" component={About} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/sharks" component={Sharks} />
+              </Switch>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.get("/api/sharks", function(req, res) {
+  console.log('hit /api/sharks');
+  res.json([
+    {type: "Whale Shark", length: "9.0m", image: "https://gulfnews.com/polopoly_fs/1.1679171!image/3382532533.gif_gen/derivatives/landscape_422/3382532533.gif"}, 
+    {type: "tiger Shark", length: "5.5m", image: "https://gulfnews.com/polopoly_fs/1.1679170!image/4285458110.gif_gen/derivatives/landscape_422/4285458110.gif"},
+    {type: "Great hammerhead", length: "3.5m", image: "https://gulfnews.com/polopoly_fs/1.1679160!image/1233273565.gif_gen/derivatives/landscape_422/1233273565.gif"}
 
+  ]);
+  res.end();
+})
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
