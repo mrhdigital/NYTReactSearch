183ffdfeb7ed4aa3d318f8e1575d34ce558aa8bb
unknown <mrhdigital@yahoo.com>
2018-07-30 14:42:18 -0500

    boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./components/About";
-import Sharks from "./components/Sharks";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div class="container">
-          <div class="row">
-            <div class="col-12">
-              <Switch>
-                <Route exact path="/" component={About} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/sharks" component={Sharks} />
-              </Switch>
-            </div>
-          </div>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
         </div>
-      </Router>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
